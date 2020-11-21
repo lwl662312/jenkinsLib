e2c2334393784044a3677bbdae933b78c416cdeb
linweilong <815192587@qq.com>
2020-11-21 16:07:25 +0800

    delete files

--- build.groovy
+++
@@ -1,7 +0,0 @@
-def Build(type, command){
-  def buildTools = ["mvn": "Maven", "ant": "Ant", "gradle": "Gradle", "npm": "Npm"]
-  
-  buildPath = tool buildTools[type]
-  
-  sh "${buildPath}/bin/${type} ${command}"
-}
--- org/devops/build.groovy
+++
@@ -1,9 +0,0 @@
-package org.devops
-
-def Build(type, command){
-  def buildTools = ["mvn": "Maven", "ant": "Ant", "gradle": "Gradle", "npm": "Npm"]
-  
-  buildPath = tool buildTools[type]
-  
-  sh "${buildPath}/bin/${type} ${command}"
-}
